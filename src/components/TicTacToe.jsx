65df7a47a1d978d9dad61d40d1beb9ee6e3af259
raghavbud1597 <raghav1597@gmail.com>
2024-06-01 08:19:29 -0400

    Adding Stats to the tic tac toe

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -2,21 +2,25 @@ import React from 'react'
 import useTicTacToe from '../hooks/useTicTacToe';
 
 const TicTacToe = () => {
-  const { board, gameStatus, handleClick, handleReset, calculateWinner } = useTicTacToe();
+  const { board, gameStatus, handleClick, handleReset, gameStats } = useTicTacToe();
   
   
   return (
     <div className='game'>
       <div className='game-controls'>
-        <span>{gameStatus()}</span>
+        <span>{ gameStatus}</span>
         <button className='reset-btn' onClick={handleReset}>Reset</button>
       </div>
       <div className='board'>
         {board.map((cell, index) => {
-          return <button className='board-cell' key={index} onClick={() => handleClick(index)}>{cell}</button>
+          return <button className='board-cell' disabled={cell !== null} key={index} onClick={() => handleClick(index)}>{cell}</button>
         })}
       </div>
-      
+      <div className='game-stats'>
+        <div>Total Number of Games - {gameStats.total}</div>
+        <div>Number of Games Won by X - {gameStats.x}</div>
+        <div>Number of Games Won by O - {gameStats.o}</div>
+      </div>
     </div>
   )
 }
--- src/hooks/useTicTacToe.jsx
+++ src/hooks/useTicTacToe.jsx
@@ -2,8 +2,11 @@ import { useState } from "react";
 
 const useTicTacToe = () => {
     let initialBoard = Array(9).fill(null);
+
     const [board, setBoard] = useState(initialBoard);
     const [xIsNext, setXIsNext] = useState(true);
+    const [gameStats, setGameStats] = useState({total:0, x:0, o:0});
+    const [gameStatus, setGameStatus] = useState("Next player: X");
 
     const winningCombos = [
         [0, 1, 2],
@@ -17,33 +20,59 @@ const useTicTacToe = () => {
     ]
 
     const handleClick = (index) => {
-        debugger;
+        const winner = calculateWinner([...board]);
+        if (winner || board[index]) {
+            return;
+        }
         let newBoard = [...board];
         newBoard[index] = xIsNext ? "X" : "O";
         setXIsNext(!xIsNext);
         setBoard(newBoard);
+        handleGameStatus(newBoard);
     };
+
     const handleReset = () => {
         setBoard(initialBoard);
         setXIsNext(true);
+        setGameStatus("Next player: X");
     };
-    const gameStatus = () => {
-        const winner = calculateWinner();
-        if (winner) {
-            return `Winner: ${winner}`;
-        } else {
-            return `Next player: ${xIsNext ? "X" : "O"}`;
+
+    const handleGameStats = (winner) => {
+        let newGameStats = {...gameStats,};
+        newGameStats.total++;
+        if(winner) {
+            winner === "X" ? newGameStats.x++ : newGameStats.o++;
         }
+        setGameStats(newGameStats);
     };
-    const calculateWinner = () => {};
+    
+    const handleGameStatus = (newBoard) => {
+        const winner = calculateWinner(newBoard);
+        if (winner) {
+            handleGameStats(winner);
+            return setGameStatus(`Winner: ${winner}`);
+        }
+        if(!newBoard.includes(null)) {
+            handleGameStats(null);
+            return setGameStatus("It's a draw!");
+        }
+        return setGameStatus(`Next player: ${!xIsNext ? "X" : "O"}`);
+    };
+
+    const calculateWinner = (board) => {
+        for (let i = 0; i < winningCombos.length; i++) {
+            const [a, b, c] = winningCombos[i];
+            if (board[a] && board[a] === board[b] && board[a] === board[c]) return board[a];
+        }
+        return null;
+    };
 
     return {
         board,
         gameStatus,
-        xIsNext,
         handleClick,
         handleReset,
-        calculateWinner
+        gameStats
     };
 }
 
